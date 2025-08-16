3385b3b89f21f517b1511f9bf4c6218b13ddb66f
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-16 15:27:48 +0530

    code pushed

--- js/basic.js
+++ js/basic.js
@@ -5,11 +5,42 @@
 //declare => var/let/const variable_name
 var a;
 let b;
-const c=45;
+const c = 45;
 //naming rules =>
     //1.can contains alphabets , numbers, $ , _ (underscore)
 //cannot start with no.
-//cant use reserve keyword as if,else , for, 
+//cant use reserve keyword as if,else , for
+//data types => 
+    // primitive and non-primitive/derived/refernce 
+//primitive 
+//1. number => interger and float 
+let d = 56;
+// console.log(d)
+// console.log(typeof d)
+//2.string => normal text (collection of characters)
+let e = 'hello'
+let f = "world"
+// console.log(e,f, typeof e)
+//3.boolean => true / false 
+let g = true
+// console.log(g,typeof g)
+//4.undefined =>
+let h;
+// console.log(h, typeof h)
+//5. null =>
+let i = null
+// console.log(i, typeof null)
+//6.bigInt => 
+let j = 343545455565656n
+// console.log(j, typeof j)
+//symbol => 
+let s1 = Symbol('a')
+let s2 = Symbol('a')
+console.log(s1 == s2)
+
+
+
+
 
 
 
