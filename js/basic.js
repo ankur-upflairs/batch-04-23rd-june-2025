0ec5e2695071d31635f8a2ec0e3e91eb5ae2e781
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-16 14:39:16 +0530

    code pushed

---
+++ js/basic.js
@@ -0,0 +1,15 @@
+// let b =45;
+// console.log(b)
+//varibale =>it is a container that stores a value 
+//it a name of memory location that stores value 
+//declare => var/let/const variable_name
+var a;
+let b;
+const c=45;
+//naming rules =>
+    //1.can contains alphabets , numbers, $ , _ (underscore)
+//cannot start with no.
+//cant use reserve keyword as if,else , for, 
+
+
+
