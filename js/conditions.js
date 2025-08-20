dbd08ae836e3964fa95dffcdeac26102f22f5a77
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-20 17:29:50 +0530

    code pushed

--- js/conditions.js
+++ js/conditions.js
@@ -12,14 +12,82 @@
 // age = parseInt(age)
 // console.log(age,typeof age)
 //parseInt , parseFloat , Number 
-let age = parseInt(prompt('enter age : '))
-if(age >= 18) {
-    // console.log('your are adult')
-    document.write('you are a adult')
+// let age = parseInt(prompt('enter age : '))
+// if(age >= 18) {
+//     // console.log('your are adult')
+//     document.write('you are a adult')
+// }
+// else {
+//     // console.log('your are minor')
+//     document.write('you are a minor')
+// }
+
+// find the no is positive / negetive / zero  
+// let n = parseInt(prompt('enter a no. : '))
+// if(n >= 0 ){
+//     console.log('n is positive')
+// }else {
+//     console.log('n is negetive')
+// }
+//if-else-if ladder 
+// let n = parseInt(prompt('enter a no. : '))
+// if(n>0){
+//     console.log('n is positive')
+// }
+// else if(n== 0){
+//     console.log('n is zero')
+// }
+// else{
+//     console.log('n is negetive ')
+// }
+//percentage (0-100) => A>90 & b> 75 & c >50 & fail < 50
+//alert('massage')
+// let n = parseInt(prompt('enter your percentage'))
+// if(n <=100 && n >90){
+//     alert('your grade is A')
+// }
+// else if(n <=90 && n >75){
+//     alert('your grade is B')
+// }
+// else if(n <=75 && n >50){
+//     alert('your grade is C')
+// }
+// else if(n <=50 && n >=0){
+//     alert('you are fail')
+// }
+// else {
+//     alert('wrong percentage')
+// }
+// let isAdult = confirm('are you 18+ ?')
+// if(isAdult){
+//     alert('you are welcome to our site.')
+// }
+// else {
+//     alert('you are not allowed to visit this site.')
+// }
+
+// find n is p/ n /z
+//nested if-else 
+let n = parseInt(prompt('enter a no. : '))
+if(n>= 0){
+    if(n>0){
+        alert('n is positive')
+    }else {
+        alert('n is zero')
+    }
+}else {
+    alert('n is negetive')
 }
-else {
-    // console.log('your are minor')
-    document.write('you are a minor')
-}
+
+
+
+
+
+
+
+
+
+
+
 
 
